c41ac551252de1cabc83dd703311cb5ebde9f9bf
Zhanna Anikanova <whiteredfoxx@gmail.com>
2021-02-13 03:15:12 +0300

    feat:первая версия игры

---
+++ class.js
@@ -0,0 +1,60 @@
+class BollModel {
+    constructor(display_width, display_height, r, dy) {
+        this.width = display_width;
+        this.height = display_height;
+        this.y = this.height;
+        this.x = Math.random() * (this.width - r * 2) + r;
+        this.r = Math.random() * r / 2 + r / 2;
+        this.color = color[Math.floor(Math.random() * color.length)];
+        this.dy = Math.random() * 3 * dy / 4 + dy / 4;
+        this.dx = 0;
+    }
+
+    update() {
+        this.y -= this.dy
+    }
+
+    windUpdate(wind) {
+        if (this.x >= 3 * this.width / 4) {
+            this.dx = wind[3];
+        }
+        if (this.x >= this.width / 2 && this.x < 3 * this.width / 4) {
+            this.dx = wind[2];
+        }
+        if (this.x >= this.width / 4 && this.x < this.width / 2) {
+            this.dx = wind[1];
+        }
+        if (this.x < this.width / 4) {
+            this.dx = wind[0];
+        }
+        if ((this.x += this.dx) < this.r) {
+            this.x = this.r
+        } else if ((this.x += this.dx) > (this.width - this.r)) {
+            this.x = this.width - this.r
+        } else {
+            this.x += this.dx
+        }
+    }
+}
+
+class Needle {
+    constructor(display_width, weight, length) {
+        this.width = display_width;
+        this.length = -length;
+        this.weight = weight;
+        this.y = 5 + length;
+        this.x = this.width / 2
+        this.color = 'silver'
+    }
+
+    moveLeft() {
+        this.x -= 6;
+        if (this.x <= 0) this.x = this.width - 1
+    }
+
+    moveRight() {
+        this.x += 6;
+        if (this.x >= this.width) this.x = 1
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,149 @@
+const color = ['mediumspringgreen', "dodgerblue", 'aqua', 'seagreen', 'greenyellow', 'yellow', 'darkturquoise', 'teal', 'cyan']
+const radius = 24;
+const rate = 5;
+const canvasWidth = 400, canvasHeight = 580;
+let canvas = document.querySelector('.canvas');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+let context = canvas.getContext('2d');
+const startBtn = document.querySelector('.start');
+const playScore = document.querySelector('.score');
+const timeScore = document.querySelector('.time');
+const gameOver = document.querySelector('.table_position_center')
+let time = 60;
+let scoreWin = 0;
+let scoreLose = 0;
+let bolls = [];
+let wind = [];
+let needle = new Needle(canvasWidth, 4, 35)
+let generateBollsInterval = setInterval(() => bolls.push(new BollModel(canvasWidth, canvasHeight, radius, rate)), 3000);
+let checkBoomInterval = setInterval(() => {
+    bolls.forEach((boll, index, array) => {
+        if (isBoom(boll, needle)) {
+            array[index] = new BollModel(canvasWidth, canvasHeight, radius, rate);
+            scoreWin++;
+            renewScore();
+        }
+        if (isPass(boll)) {
+            array[index] = new BollModel(canvasWidth, canvasHeight, radius, rate);
+            scoreLose++;
+            console.log(scoreLose)
+        }
+    })
+}, 40)
+
+//массив скоростей ветра
+let windInterval = setInterval(() => {
+    let v = Math.random() * 2;
+    const symbol = Math.random() * 2;
+    if (symbol < 1) {
+        v = -v;
+        wind[3] = v;
+        for (let i = 4; i >1; i--) {
+            wind[4-i] = v / i/2
+        }
+    } else {
+        wind[0] = v;
+        for (let i = 2; i < 5; i++) {
+            wind[i - 1] = v / i/2
+        }
+    }
+    console.log(wind)
+    return wind
+}, 2000)
+
+
+function renewScore() {
+    playScore.textContent = `${scoreWin}`;
+}
+
+//проверка прокола шара
+function isBoom(boll, needle) {
+    return Math.sqrt((needle.x - boll.x) ** 2 + (needle.y - boll.y) ** 2) <= boll.r;
+}
+
+//проверка, что шарик прошел мимо иглы
+function isPass(boll) {
+    return boll.y < -2 && boll.y > -6
+}
+
+function generateBolls() {
+    return generateBollsInterval
+}
+
+function checkBoom() {
+    return checkBoomInterval
+}
+
+function windImitation() {
+    return windInterval
+}
+
+function updateBolls() {
+    bolls.forEach(boll => {
+        boll.update();
+        boll.windUpdate(wind);
+    });
+}
+
+//обновление изображения canvas
+function render() {
+    updateBolls();
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    context.fillStyle = needle.color;
+    context.fillRect(needle.x, needle.y, needle.weight, needle.length);
+
+    bolls.forEach(boll => {
+        context.fillStyle = boll.color;
+        context.beginPath();
+        context.arc(boll.x, boll.y, boll.r, 0, 2 * Math.PI, false);
+        context.fill();
+    })
+}
+
+// меняет положение иглы при нажатии стрелок влево/вправо
+function needleMove(evt) {
+    if (evt.key === 'ArrowLeft') return needle.moveLeft()
+    if (evt.key === 'ArrowRight') return needle.moveRight()
+}
+
+//останавливает игру по истечении time
+function stop() {
+    document.removeEventListener('keydown', needleMove);
+    clearInterval(generateBollsInterval);
+    clearInterval(checkBoomInterval);
+    clearInterval(windInterval);
+    setInterval(render, 5);
+
+    gameOver.style.display = 'block';
+    gameOver.textContent = `Время истекло. Лопнуто: ${scoreWin}. Пропущено: ${scoreLose}`
+}
+
+// Если ускорять под конец, тогда нужно для геренерации шаров и для рендера рекурсивно вызывать setTimeout (как
+// сделано со временем) и уменьшать delay в каждой итерации.
+function start() {
+    startBtn.style.display = 'none';
+    renewScore();
+    //добавляет кнопке возможность перемащаться влево/вправо
+    document.addEventListener('keydown', needleMove);
+    generateBolls();
+    checkBoom();
+    windImitation()
+    setInterval(render, 40);
+    //счетчик обратного отсчета времени
+    let timerId = setTimeout(function renewTime() {
+        time--;
+        timeScore.textContent = `${time}`;
+        if (time > 0) {
+            timerId = setTimeout(renewTime, 1000);
+        } else {
+            stop()
+        }
+    }, 1000)
+}
+
+//запускает игру при нажатии кнопки старт
+startBtn.addEventListener('click', start)
+
+
